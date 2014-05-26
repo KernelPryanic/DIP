f368c7c2452e06ad52a18373dad1133438f00ab7
FromZeus <asteroid56@yandex.ru>
2014-05-26 23:26:37 +0400

    Kernel neural net are there!

---
+++ KernelNeuroNet/KernelNeuroNet/MyClass.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace KernelNeuroNet
+{
+	public class KernelNet
+	{
+		public KernelNet()
+		{
+
+		}
+	}
+}
+
